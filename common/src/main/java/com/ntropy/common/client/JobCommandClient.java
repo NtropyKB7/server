987c7f1c7252c30631d92a56dcbb9fbc0e226630
JaeYoon <hanjyoon01@gmail.com>
2026-07-30 10:19:27 +0900

    feat: JobCommandClient 및 등록/수정 DTO 구현
    - common: JobRegisterCommand/JobUpdateCommand/JobScheduleCommand, JobCommandClient 인터페이스 정의
    - work-service: LocalJobCommandClient 구현 (등록 시 플랫폼 선택하면 JobPlatformMapping도 같이 등록)

---
+++ common/src/main/java/com/ntropy/common/client/JobCommandClient.java
@@ -0,0 +1,17 @@
+package com.ntropy.common.client;
+
+import com.ntropy.common.dto.JobRegisterCommand;
+import com.ntropy.common.dto.JobUpdateCommand;
+
+/**
+ * work-service의 JOB 쓰기 계약. work-service가 LocalJobCommandClient로 구현하고,
+ * bff-service 등 다른 서비스는 이 인터페이스만 의존한다.
+ */
+public interface JobCommandClient {
+
+    Long registerJob(JobRegisterCommand command);
+
+    void updateJob(Long jobId, JobUpdateCommand command);
+
+    void deactivateJob(Long jobId);
+}
