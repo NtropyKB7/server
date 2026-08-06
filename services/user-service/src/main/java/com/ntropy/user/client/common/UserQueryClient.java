639b131565fa094dfd968c19cdd2c71d355c92c3
JaeYoon <hanjyoon01@gmail.com>
2026-08-06 17:20:34 +0900

    refactor: user-service 모듈 간 계약을 common으로 이동
    - UserQueryClient, UserSummary를 common으로 이동
      (user-service 내부 client/common, dto/common 패키지 제거)
    - UserSummary를 Lombok @Builder에서 record로 전환 (common DTO 컨벤션)
    - TokenVerifier, VerifiedToken 계약 신규 정의
    - LocalTokenVerifier 구현 (JwtProvider 위임, 파싱 예외를 null로 흡수)
    - LocalUserQueryClient의 @Service를 @Component로 변경

--- services/user-service/src/main/java/com/ntropy/user/client/common/UserQueryClient.java
+++
@@ -1,8 +0,0 @@
-package com.ntropy.user.client.common;
-
-import com.ntropy.user.dto.common.UserSummary;
-
-// 나중에 common/src/main/java/com/ntropy/common/client/ 로 이동할 파일
-public interface UserQueryClient {
-    UserSummary getUserSummary(Long userId);
-}
