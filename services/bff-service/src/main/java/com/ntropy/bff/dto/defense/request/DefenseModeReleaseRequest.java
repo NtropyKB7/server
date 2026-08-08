ffe26f55085e3c5e0f306e2c0ffb4ba80d70e31b
ShinHyung Park <161359299+shinh09@users.noreply.github.com>
2026-08-08 15:47:12 +0900

    refactor: 방어모드 요청 userId를 제거 (#100)

--- services/bff-service/src/main/java/com/ntropy/bff/dto/defense/request/DefenseModeReleaseRequest.java
+++ services/bff-service/src/main/java/com/ntropy/bff/dto/defense/request/DefenseModeReleaseRequest.java
@@ -9,10 +9,9 @@ import java.time.LocalDate;
 @Getter
 @NoArgsConstructor
 public class DefenseModeReleaseRequest {
-    private Long userId;
     private LocalDate returnDate;
 
-    public DefenseModeReleaseCommand toCommand() {
+    public DefenseModeReleaseCommand toCommand(Long userId) {
         return new DefenseModeReleaseCommand(userId, returnDate);
     }
 }
